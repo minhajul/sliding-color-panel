13c0598537fe10557a411cc0f9009b7adb703a90
minhajul <kmsohelinfo@gmail.com>
2025-02-26 13:13:30 +0600

    Updated

--- src/alpine-color-picker.js
+++ src/alpine-color-picker.js
@@ -1,17 +1,35 @@
 export default function ColorPickerPlugin(Alpine) {
     Alpine.data('colorPicker', (config = {}) => ({
         isPanelOpen: false,
-        selectedPrimary: config.initialPrimary || 'teal-600',
-        selectedSecondary: config.initialSecondary || 'teal-400',
+        selectedPrimary: config.initialPrimary || 'cyan-200',
+        selectedSecondary: config.initialSecondary || 'indigo-300',
         primaryColors: config.primaryColors || [
-            'teal-600', 'orange-500', 'pink-500', 'blue-600',
-            'teal-400', 'green-400', 'lime-400', 'yellow-400',
-            'orange-600', 'black', 'green-600', 'gray-600'
+            'teal-200',
+            'orange-200',
+            'pink-200',
+            'blue-200',
+            'cyan-200',
+            'green-200', 
+            'lime-200', 
+            'yellow-200',
+            'orange-200', 
+            'black', 
+            'green-200', 
+            'gray-200'
         ],
         secondaryColors: config.secondaryColors || [
-            'red-400', 'orange-300', 'yellow-300', 'lime-300',
-            'green-300', 'blue-400', 'indigo-400', 'purple-400',
-            'purple-600', 'pink-400', 'gray-800', 'gray-400'
+            'red-300', 
+            'orange-300', 
+            'yellow-300', 
+            'lime-300',
+            'green-300', 
+            'blue-300', 
+            'indigo-300', 
+            'purple-300',
+            'cyan-300',
+            'pink-300', 
+            'gray-300',
+            'gray-900'
         ],
         togglePanel() {
             this.isPanelOpen = !this.isPanelOpen;
